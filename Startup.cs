92006a7ef86bbe01b34e3961db82867fb748d415
Carleton Cabarrus <carleton.cabarrus@onshoreoutsourcing.com>
2019-08-16 16:10:11 -0500

    Swagger with EF creates swagger documents based on EF controller routes for an API

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BenchWarmerAPI
 {
@@ -26,6 +27,8 @@ namespace BenchWarmerAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = "BenchWarmerAPI", Version = "v1" }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +45,11 @@ namespace BenchWarmerAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BenchWarmerAPI V1");
+            });
         }
     }
 }
